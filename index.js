6f4f0b8a57a9fc4f066b2adbc22415e61f32f3e7
DeograciousAggrey <DeograciousAggrey@gmail.com>
2021-10-25 22:57:29 +0300

    Writing a Js variable

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
- document.getElementById("count-el").innerHTML = 5;
-       
+ //document.getElementById("count-el").innerHTML = 5;
+   
+ let count = 0;
+ console.log(count);
+
+ let myAge = 20;
+ console.log(myAge);
